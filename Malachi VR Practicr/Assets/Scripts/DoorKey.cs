990f004242831ea9e1614fe8aa6846663c7d7caf
SharpEye125 <creeperminer125@gmail.com>
2022-11-15 12:16:53 -0800

    Added Key and it's related Script

---
+++ Malachi VR Practicr/Assets/Scripts/DoorKey.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+    public GameObject key;
+    Rigidbody keyRB;
+    bool inLock = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyRB = key.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inLock == true)
+        {
+            if (key.transform.eulerAngles.x >= 0 || key.transform.eulerAngles.x <= -180f)
+            {
+                keyRB.constraints = RigidbodyConstraints.None;
+                key.GetComponent<PerObjectOoB>().BackToStartPos();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Key"))
+        {
+            key.transform.eulerAngles = new Vector3(-90, 180, 0);
+            keyRB.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+            keyRB.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY;
+            keyRB.constraints = RigidbodyConstraints.FreezePositionX;
+        }
+        
+    }
+}
